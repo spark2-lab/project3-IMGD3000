7561c57248ec0af04ea360b78976674593963b28
박세현 <saehyunpark@Mac.lan>
2025-10-04 22:47:37 -0400

    Changed sprites/saucer-spr.txt to pterodactyl.txt

--- game.cpp
+++ game.cpp
@@ -13,7 +13,7 @@
 void loadResources(void)
 {
   // Load saucer sprite.
-  RM.loadSprite("sprites/saucer-spr.txt", "saucer");
+  RM.loadSprite("sprites/pterodactyl-spr.txt", "saucer");
   RM.loadSprite("sprites/dino-spr.txt", "dino");
   RM.loadSprite("sprites/bullet-spr.txt", "bullet");
   RM.loadSprite("sprites/explosion-spr.txt", "explosion");
